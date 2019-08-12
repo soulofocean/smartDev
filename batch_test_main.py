a64d5613eb7660ce83252181c8ba8817d9275b3f
HD-NoteBook <251455882@qq.com>
2019-08-12 20:16:57 +0800

    调整默认配置

--- batch_test_main.py
+++ batch_test_main.py
@@ -12,7 +12,7 @@ monkey.patch_all()
 # 每个进程启动时间间隔 单位秒
 process_start_delay = 0.1
 # 监控打印间隔，时间秒
-monitor_inv_sec = 5
+monitor_inv_sec = 300
 dev_config = [
     {
         # 可执行程序所在文件夹
@@ -24,17 +24,17 @@ dev_config = [
         # 网关IP和端口，类型是一个元组
         "server_addr": ("10.101.70.100",2001),
         # 启动的设备数目
-        "dev_count": 200,
+        "dev_count": 1,
         # 指定模拟器的配置文件，需要和protocol下的config文件对应
         "config_file": "door_conf",
         # 注册等待时间,单位秒
-        "send_pkt_delay": 20,
+        "send_pkt_delay": 30,
         # 发包超时时间：【0：发完包后收包==发包退出，-1：即使发完包也永远循环不退出，N：运行超过N秒后就退出】
         "send_pkt_timeout": 0,
         # 发包间隔 单位秒
         "pkt_period_s": 1,
         # 一个进程包含的协程数,由于用了Select，目前不能超过500
-        "max_thread_count": 2,
+        "max_thread_count": 500,
         # 起始设备ID偏移量，默认为0
         "default_offset": 0,
     },

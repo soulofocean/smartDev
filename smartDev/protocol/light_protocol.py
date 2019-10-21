e81af457ac865b45893bb316cb6f0f22d53567ae
HD-NoteBook <251455882@qq.com>
2019-10-21 15:30:11 +0800

    打印报表添加时延计算逻辑和在线设备数统计

--- smartDev/protocol/light_protocol.py
+++ smartDev/protocol/light_protocol.py
@@ -6,17 +6,10 @@ by Kobe Gong. 2018-1-15
 """
 
 import asyncio
-import binascii
-import datetime
 import json
-import logging
-import os
-import random
 import re
 import struct
-import sys
 import time
-from abc import ABCMeta, abstractmethod
 from collections import defaultdict
 
 import APIs.common_APIs as common_APIs
@@ -40,6 +33,7 @@ class SDK(asyncio.Protocol):
         self.device_id = b'\x31\x30\x30\x34\x32\x30\x31\x36\x35\x38\x46\x43\x44\x42\x44\x38\x33\x34\x31\x45'
         # 4bytes
         self.pkg_number = b'\x00\x00\x00\x01'
+        self.msgst = defaultdict(lambda: {})
 
     def connection_made(self, transport):
         self.transport = transport
@@ -48,15 +42,40 @@ class SDK(asyncio.Protocol):
 
     def data_received(self, data):
         self.queue_in.put_nowait(data)
-        self.LOG.debug(protocol_data_printB(data,title="client get data:"))
-        self.LOG.debug(repr(data))
+        self.LOG.debug(protocol_data_printB(data, title="client get data:"))
+        command = self.get_cmd_from_pkt(data)
+        self.LOG.debug("RECV_CMD:{}".format(command))
+        if command and b'"Result":' in data:
+            # 收到的带有回复的才去计算时延，否则计算时延无意义
+            self.init_msgst_dict(command)
+            self.msgst[command]['time_info'].recv_update()
+        self.LOG.debug("RECV:{}".format(data.__repr__()))
 
     def connection_lost(self, exc):
         self.LOG.critical('The server closed the connection!')
+        if exc:
+            self.LOG.critical('[{}]connection_lost exc:{}'.format(type(exc), repr(exc)))
         self.dev_register = False
+        loop = asyncio.get_event_loop()
+        while True:
+            try:
+                # print(time.time())
+                peer_info = self.transport.get_extra_info('socket').getpeername()
+                if peer_info and len(peer_info) == 2:
+                    # print('peer_info:',peer_info)
+                    # 10秒后重连
+                    time.sleep(10)
+                    # 此处出了异常很可能不会捕获到，想要捕获到需要研究或者试试直接操作get_extra_info获取到的socket对象进行重连？
+                    asyncio.create_task(loop.create_connection(lambda: self, peer_info[0], peer_info[1]))
+                else:
+                    raise AssertionError("get peer_info error.")
+            except Exception as ex:
+                print("EX:{}".format(ex.__repr__()))
+            else:
+                break
 
     def msg_build(self, data, ack=b'\x01'):
-        #self.LOG.info("send msg: " + self.convert_to_dictstr(data))
+        # self.LOG.info("send msg: " + self.convert_to_dictstr(data))
         # need encrypt
         if self.encrypt_flag:
             data = AES_CBC_encrypt(self._encrypt_key, data)
@@ -105,7 +124,7 @@ class SDK(asyncio.Protocol):
                         data_list += data_list_tmp
                         left_data += left_data_tmp
                     else:
-                        #self.LOG.error('data field is empty!')
+                        # self.LOG.error('data field is empty!')
                         pass
                 elif length >= 1 and length < 10000:
                     left_data = data
@@ -135,7 +154,8 @@ class SDK(asyncio.Protocol):
     def protocol_handler(self, msg):
         ack = False
         if msg[0:4] == b'\x48\x44\x58\x4d':
-            if True or msg[4 + 20:4 + 20 + 20] == self.device_id or msg[4 + 20:4 + 20 + 20] == self.device_id.encode('utf-8'):
+            if True or msg[4 + 20:4 + 20 + 20] == self.device_id or msg[4 + 20:4 + 20 + 20] == self.device_id.encode(
+                    'utf-8'):
                 if msg[44:45] != b'\x00':
                     ack = True
 
@@ -165,7 +185,7 @@ class SDK(asyncio.Protocol):
             return "No_need_send"
 
     async def schedule(self):
-        while self.getStopConition()==False:
+        while self.getStopConition() == False:
             if self.queue_in.empty():
                 pass
             else:
@@ -176,6 +196,7 @@ class SDK(asyncio.Protocol):
                 if data_list:
                     for request_msg in data_list:
                         # print("rsv:{}".format(request_msg))
+                        # self.LOG.info("rsv:{}".format(request_msg))
                         response_msg = self.protocol_handler(request_msg)
                         if response_msg == 'No_need_send':
                             pass
@@ -197,19 +218,25 @@ class SDK(asyncio.Protocol):
         else:
             data = await self.queue_out.get()
             self.transport.write(data)
-            self.LOG.debug(protocol_data_printB(data, title="client send data:"))
+            self.LOG.info(protocol_data_printB(data, title="client send data:"))
             self.LOG.info(repr(data))
 
     async def send_data_loop(self):
-        while self.getStopConition()==False:
+        while self.getStopConition() == False:
             if self.queue_out.empty():
                 pass
             else:
                 data = await self.queue_out.get()
+                command = self.get_cmd_from_pkt(data)
+                self.LOG.debug("SEND_CMD:{}".format(command))
+                if command and b'"Result":' not in data:
+                    # 发送ACK包的时候不需要计算时延，也不需要重置发送时间
+                    self.init_msgst_dict(command)
+                    self.msgst[command]['time_info'].send_update()
                 # print("send:{}".format(data))
                 self.transport.write(data)
                 self.LOG.debug(protocol_data_printB(data, title="client send data:"))
-                self.LOG.info(repr(data))
+                self.LOG.debug("SEND:{}".format(repr(data)))
             await asyncio.sleep(self.procInv)
 
     def to_send_data(self, data):
@@ -223,8 +250,30 @@ class SDK(asyncio.Protocol):
             return json.dumps(json.loads(src), sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
 
         elif isinstance(src, bytes):
-            return json.dumps(json.loads(src.decode('utf-8')), sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
+            return json.dumps(json.loads(src.decode('utf-8')), sort_keys=True, indent=4, separators=(',', ': '),
+                              ensure_ascii=False)
 
         else:
             self.LOG.error('Unknow type(%s): %s' % (src, str(type(src))))
             return None
+
+    def get_cmd_from_pkt(self, pkt: bytes):
+        try:
+            m = re.search(b'"Command":.*?"(\w+)"', pkt)
+            if m:
+                return m.group(1)
+            return None
+        except Exception as e:
+            self.LOG.error("get_cmd_from_pkt exception:{}".format(repr(e)))
+            return None
+
+    def init_msgst_dict(self, command):
+        if command in self.msgst:
+            pass
+        else:
+            self.msgst[command] = {
+                'req': 0,
+                'rsp': 0,
+                'rsp_fail': 0,
+                'time_info': common_APIs.msgst_time_info()
+            }
